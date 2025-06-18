27645c0d7ddb4790049385adb8a5112223caebab
Pedro Noya <pedrodefreitasnoya@gmail.com>
2025-06-18 11:11:26 +0100

    inicio

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Portal Colaborador</title>
+</head>
+<body>
+    <a href="updateInfo.php">
+        <button type="button">Update Info</button>
+    </a>
+</body>
+</html>
