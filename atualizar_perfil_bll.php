f2632eddcd129a45b614f552be0d718cc7d1a1d4
Tiago Almeida <1222066@isep.ipp.pt>
2025-06-25 09:36:07 +0100

    Estilo adicionado à página

--- atualizar_perfil_bll.php
+++ atualizar_perfil_bll.php
@@ -23,14 +23,19 @@ function showForm($dados){
         Nome Completo: <input type="text" name="nome" placeholder="Nome Completo" value="',$dados["nome"],'" readonly required><br>
         Nome Abreviado: <input type="text" name="nomeAbreviado" placeholder="ex: António Silva" value="',$dados["nomeAbreviado"],'"readonly required><br>
         </div>
+        <div class="container">
         Data de Nascimento: <input type="date" name="dataNascimento" value="',$dados["dataNascimento"],'" readonly required><br>
-        Número de Identificação Fiscal: <input type="text" name="nif" placeholder="NIF" value="',$dados["nif"],'" readonly required><br>
-        Número de Segurança Social: <input type="text" name="niss" placeholder="NISS" value="',$dados["niss"],'" readonly required><br>
-        Número Cartão de Cidadão: <input type="text" name="cc" placeholder="CC" value="',$dados["cc"],'" readonly required><br>';
+        </div>
+        <div class="container">
+        Número de Identificação Fiscal: <input type="text" name="nif" placeholder="NIF" value="',$dados["nif"],'" readonly required>
+        Número de Segurança Social: <input type="text" name="niss" placeholder="NISS" value="',$dados["niss"],'" readonly required>
+        Número Cartão de Cidadão: <input type="text" name="cc" placeholder="CC" value="',$dados["cc"],'" readonly required>
+        </div>';
 
         $genero_array=["M","F"];
         #É correr o array genero_array e ver em qual indice o valor do genero é igual ao genero do colaborador em questão.
-        echo 'Sexo: <select name="genero" required>';
+        echo '<div class="container">
+        Sexo: <select name="genero" required>';
         foreach ($genero_array as $genero){
             if($genero==$dados["genero"]){
                 echo '<option value="',$genero,'" selected>',$genero,'</option>';
@@ -39,11 +44,12 @@ function showForm($dados){
             }            
         }
         echo '</select><br>';
-        echo 'Nacionalidade: <input type="text" name="nacionalidade" placeholder="Nacionalidade" value="',$dados["nacionalidade"],'" readonly required><br>';
-        
+        echo 'Nacionalidade: <input type="text" name="nacionalidade" placeholder="Nacionalidade" value="',$dados["nacionalidade"],'" readonly required><br>
+        </div>';
         $situacaoIrs_array=["Declaração de IRS recepcionada","Declaração certa","Liquidação processada","Reembolso Emitido","Pagamento Confirmado","Liquidada com nota de cobrança emitida","Notificação emitida","Liquidada com Saldo Nulo Emitido"];
 
-        echo 'Situação IRS: <select name="situacaoIrs" required>';
+        echo '<div class="container">
+        Situação IRS: <select name="situacaoIrs" required>';
         foreach($situacaoIrs_array as $situacaoIrs){
             if($situacaoIrs==$dados["situacaoIrs"]){
                 echo '<option value="',$situacaoIrs,'" selected>',$situacaoIrs,'</option>';
@@ -55,19 +61,21 @@ function showForm($dados){
         
         echo 'Dependentes: <input type="text" name="numDependentes" placeholder="Número de Dependentes" value="',$dados["numDependentes"],'" required><br>
         IRS Jovem: O que é isto?<br>
-        Primeiro ano de descontos (independente): O que é isto?<br>
+        Primeiro ano de descontos (independente): O que é isto?<br></div>
+        <div class="container">
         Morada: <input type="text" name="morada" placeholer="Morada, i.e. Rua, Nº da Porta" value="',$dados["rua"],', ',$dados["numPorta"],'" required><br>
         Localidade: <input type="text" name="localidade" placeholder="Localidade" value="', $dados["localidade"],'" required><br>
-        Código Postal: <input type="text" name="codPostal" placeholder="Código Postal (ex: 4320-350)" value="', $dados["codPost"],'" required><br>';
-
-        echo 'Telemóvel: <input type="text" name="contacto" placeholder="Telemóvel" value="', $dados["contacto"],'" required><br>';
+        Código Postal: <input type="text" name="codPostal" placeholder="Código Postal (ex: 4320-350)" value="', $dados["codPost"],'" required><br>
+        </div>';
+        echo '<div class="container">Telemóvel: <input type="text" name="contacto" placeholder="Telemóvel" value="', $dados["contacto"],'" required><br>';
         
         
-        echo 'Email: <input type="text" name="email" placeholder="email" value="',$dados["email"],'" readonly required><br>
-        IBAN: <input type="text" name="iban" placeholder="IBAN" value="',$dados["iban"],'" required><br>
-        Contacto de Emergência: <input type="text" name="contactoEmergencia" placeholder="Nome" value="',$dados["contactoEmergencia"],'" required><br>
+        echo 'Email: <input type="text" name="email" placeholder="email" value="',$dados["email"],'" readonly required></div>
+        <div class="container">IBAN: <input type="text" name="iban" placeholder="IBAN" value="',$dados["iban"],'" required></div><br>
+        <div class="container">Contacto de Emergência: <input type="text" name="contactoEmergencia" placeholder="Nome" value="',$dados["contactoEmergencia"],'" required><br>
         Grau de Relacionamento: <input type="text" name="grauRelacionamento" placeholder="Grau de Parentesco" value="',$dados["grauRelacionamento"],'" required><br>
         Contacto: <select
+        </div>
         <button type="submit">Login</button>
         </form>';
     }
