fae2f54a0d5e8c8b59681e75db973eafd63640c2
Tiago Almeida <1222066@isep.ipp.pt>
2025-07-03 20:08:17 +0100

    Alteração do nome da página "atualizar_perfil" para "InformacoesColaborador"

--- BLL/InformacoesColaborador_bll.php
+++ BLL/InformacoesColaborador_bll.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/../DAL/atualizar_perfil_dal.php';
+require_once __DIR__ . '/../DAL/InformacoesColaborador_dal.php';
 
 
 function obterDadosPerfil(){
--- InformacoesColaborador.php
+++ InformacoesColaborador.php
@@ -1,5 +1,5 @@
 <?php
-include "/../BLL/atualizar_perfil_bll.php";
+include "/../BLL/InformacoesColaborador_bll.php";
 ?>
 <!DOCTYPE html>
 <html lang="pt">
