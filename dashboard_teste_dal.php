528270ed8e37e3fb1b00d318b1279deb82f31eb0
Tiago Almeida <1222066@isep.ipp.pt>
2025-07-04 10:56:39 +0100

    Adição de uma página de dashboard de teste

---
+++ dashboard_teste_dal.php
@@ -0,0 +1,62 @@
+<?php
+class DAL_Dashboard {
+    private $conn;
+
+    function __construct() {
+        $this->conn = new mysqli('localhost', 'root', '', 'tlantic');
+        if ($this->conn->connect_error) {
+            die("Erro na ligação à base de dados: " . $this->conn->connect_error);
+        }
+    }
+
+    /**
+     * Obtém todos os colaboradores com dados principais
+     * Campos esperados na tabela:
+     * - nome
+     * - dataNascimento
+     * - sexo
+     * - dataEntrada (ou equivalente)
+     * - funcao
+     */
+    function obterColaboradores() {
+        $sql = "SELECT nome, dataNascimento, sexo, dataEntrada, funcao FROM Colaboradores";
+        $result = $this->conn->query($sql);
+
+        $colaboradores = [];
+
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                // Calcular idade
+                $idade = $this->calcularIdade($row['dataNascimento']);
+
+                // Calcular tempo de empresa
+                $tempo = $this->calcularAntiguidade($row['dataEntrada']);
+
+                $colaboradores[] = [
+                    'nome'   => $row['nome'],
+                    'idade'  => $idade,
+                    'genero' => strtolower($row['sexo']),
+                    'tempo'  => $tempo,
+                    'funcao' => $row['funcao']
+                ];
+            }
+        }
+
+        return $colaboradores;
+    }
+
+    private function calcularIdade($dataNascimento) {
+        $nasc = new DateTime($dataNascimento);
+        $hoje = new DateTime();
+        $idade = $hoje->diff($nasc)->y;
+        return $idade;
+    }
+
+    private function calcularAntiguidade($dataEntrada) {
+        $entrada = new DateTime($dataEntrada);
+        $hoje = new DateTime();
+        $anos = $hoje->diff($entrada)->y;
+        return $anos;
+    }
+}
+?>
